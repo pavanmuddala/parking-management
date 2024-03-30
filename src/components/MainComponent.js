9cd83e5aad467e5a068756aa9852c5ebac8d6c5d
pavanmmgs <pavanmuddala@gmail.com>
2024-03-31 01:03:38 +0530

    Navigation added to analytics image in main page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,12 +1,12 @@
-
+import Link from "next/link";
 import styles from "./MainComponent.module.css"
 const MainComponent = () => {
 
-    return <div style={{marginLeft:"200px"}}>
+    return <div style={{ marginLeft: "200px" }}>
         <div style={{ display: "flex", backgroundColor: "lightblue", justifyContent: "center", alignItems: "center", height: "60px", fontSize: "24px", fontWeight: "bold" }}>
             <div>Welcome to Parking Management System</div>
         </div>
-        <div style={{ display: "flex", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",margin:"10px" }}>
+        <div style={{ display: "flex", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", margin: "10px" }}>
             <div style={{ padding: "20px", fontStyle: "italic", fontSize: "16px" }}>
                 <p>Efficient, hassle-free, and convenient - that&apos;s what we strive to offer with our cutting-edge parking management solution. Whether you&apos;re a commercial establishment looking to optimize your parking space or a municipality aiming to streamline urban parking, we&apos;ve got you covered.</p>
 
@@ -30,16 +30,12 @@ const MainComponent = () => {
                 <div style={{ padding: "20px", fontSize: "20px", fontWeight: "bold" }}>Key Features</div>
             </div>
             <div style={{ display: "flex", flexDirection: "row", gap: "30px", padding: "20px" }}>
-                <div className={styles.card}>
-                    <div className={styles.cardHead}>Analytics</div>
-                    {/* <div className={styles.cardText}>
-                        In Analytics We provide Real time data regarding vehicles, type of vehicles and revenue generated per day
+                <Link href={'./Analytics'} className={styles.LinkStyle}>
+                    <div className={styles.card}>
+                        <div className={styles.cardHead}>Analytics</div>
+                        <img src="./images/Analytics.png" height={"130px"}></img>
                     </div>
-                    <div className={styles.cardText}>
-                        Checkout this analytics by clicking here
-                    </div> */}
-                    <img src="./images/Analytics.png" height={"130px"}></img>
-                </div>
+                </Link>
                 <div className={styles.card}>
                     <div className={styles.cardHead}>Parking Spot Allocation</div>
                     <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}><img src="./images/parkingSpot.png" height={"130px"}></img></div>
